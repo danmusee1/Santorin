b0618b023d11146a44f7dea7590847af54881d4b
danmusee1 <dmusee721@gmail.com>
2022-12-05 05:33:10 +0300

    @tailwind css forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>Santorin</title>
         <meta name="description" content="All in one" />
