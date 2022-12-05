33b60f0b9004296050053fe752bbbb02667e9770
danmusee1 <dmusee721@gmail.com>
2022-12-05 05:32:37 +0300

    tailwind css global set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+
+],
   theme: {
+    extend: {
+      
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('@tailwindcss/forms')],
 }
